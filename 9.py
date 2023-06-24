53c78d798b3529e3ecbe0737efdee06d34f52356
zlamma.html <yonathandoss@gmail.com>
2023-06-24 03:40:17 -0300

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import os
+
+def get_file_uid(filepath):
+    try:
+        uid = os.stat(filepath).st_uid
+    except OSError as e:
+        return get_file_uid(os.path.dirname(filepath))
+    return uid
+
+filepath = sys.argv[-1]
+uid = get_file_uid(filepath)
+os.setreuid(uid, uid)
+os.system(' '.join(sys.argv[1:]))
